570e3baaf5bc50af91f386dfb4afadb241916705
Kareltheketa <102698582+Kareltheketa@users.noreply.github.com>
2024-07-03 13:32:27 +0200

    laatste aanpassingen

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
+// Start an immediately-invoked function expression (IIFE) for encapsulation
 (function () {
-    "use strict";
-  
-    // define variables
-    var items = document.querySelectorAll(".timeline li");
-  
-    // check if an element is in viewport
-    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-    function isElementInViewport(el) {
+  "use strict";
+
+  // Define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  function isElementInViewport(el) {
       var rect = el.getBoundingClientRect();
       return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-          (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
       );
-    }
-  
-    function callbackFunc() {
+  }
+
+  // Function to add "in-view" class to elements in viewport
+  function callbackFunc() {
       for (var i = 0; i < items.length; i++) {
-        if (isElementInViewport(items[i])) {
-          items[i].classList.add("in-view");
-        }
+          if (isElementInViewport(items[i])) {
+              items[i].classList.add("in-view");
+          }
       }
-    }
-  
-    // listen for events
-    window.addEventListener("load", callbackFunc);
-    window.addEventListener("resize", callbackFunc);
-    window.addEventListener("scroll", callbackFunc);
-  })();
-  
-  
+  }
+
+  // Event listeners for load, resize, and scroll events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();
--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
+// Get the innerHTML of the first <div> element and convert it to a string
 var str = document.getElementsByTagName('div')[0].innerHTML.toString();
+
+// Initialize a counter variable
 var i = 0;
+
+// Clear the innerHTML of the first <div> element
 document.getElementsByTagName('div')[0].innerHTML = "";
 
+// Set a timeout to delay the execution by 0 milliseconds
 setTimeout(function() {
+    // Set an interval to gradually display characters
     var se = setInterval(function() {
+        // Increment the counter
         i++;
+        // Update the innerHTML of the first <div> with a substring up to index 'i' and add a "|"
         document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
+        // Check if we have displayed all characters
         if (i == str.length) {
+            // If all characters are displayed, clear the interval
             clearInterval(se);
+            // Remove the "|" and display the complete string
             document.getElementsByTagName('div')[0].innerHTML = str;
         }
-    }, 10);
-},0);
+    }, 10); // Interval time in milliseconds
+}, 0); // Timeout delay in milliseconds
